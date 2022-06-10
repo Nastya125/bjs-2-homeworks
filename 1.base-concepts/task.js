ac5beba0fdcc9ffb22031c9b86c6b37e06d7810c
Nastya125 <101986602+Nastya125@users.noreply.github.com>
2022-06-10 14:45:52 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -6,10 +6,7 @@ function solveEquation(a, b, c) {
   let arr;
   let d = Math.pow(b, 2) - (4 * a * c);
 
-  if (d < 0){
-    arr = [];
-    
-  } else if (d === 0){
+   if (d === 0){
     arr = [-b / (2 * a)]
 
     
@@ -24,8 +21,6 @@ function solveEquation(a, b, c) {
 
 }
 
-
-"use strict"
 
 
 let diffInMonth;
