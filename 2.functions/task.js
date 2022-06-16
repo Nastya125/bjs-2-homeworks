228ee33e3a82221bff84657f81a52a41358ae96f
Nastya125 <nastyatrue@mail.ru>
2022-06-16 19:58:33 +0300

    1 версия

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,11 +1,36 @@
 // Задание 1
+let min, max, sum, avg;
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  
+  al = arr.length;
+ 
+  min = arr[al-1];
+  max = arr[al-1];
 
-  // Ваш код
+    while (al--){
+        if(arr[al] < min){
+            min = arr[al]
+        };
+        if(arr[al] > max){
+          max = arr[al]
+      };
+    };
 
-  return { min: min, max: max, avg: avg };
-}
+      let array = arr
+      sum = 0;
+      for (let i = 0; i < array.length; i++ ){ 
+        sum += array[i];
+      }
+      
+      avg = sum / array.length; 
+
+    }
+    
+
+              return {min: min, max: max, avg: avg };
+              getArrayParams([21, 50, -5, 10])   
+
+            
 
 // Задание 2
 function worker(arr) {
