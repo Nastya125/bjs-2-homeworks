5ffc9b2b24823ad296f012ef3ffc9a3cbce1fb17
Nastya125 <nastyatrue@mail.ru>
2022-06-22 21:41:09 +0300

    2 задания

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,20 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  
+  result = arr1.length === arr2.length && arr1.every((number, index) => arr2[index] === number);
   return result; // boolean
 }
+
+
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(number => number > 0);
+  resultArr = resultArr.filter(number => number % 3 === 0);
+  resultArr = resultArr.map(number => number * 10);
 
   return resultArr; // array
 }
+
+
