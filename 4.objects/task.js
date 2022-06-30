8e75731815da910a539760ea514c7355b946dfc1
Nastya125 <nastyatrue@mail.ru>
2022-07-01 00:11:16 +0300

    Пуш 1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
+
+let student1 = new Student("Alesha", "male", 28);
+let student2 = new Student("Nastusha", "female", 28);
+let student3 = new Student("Olka", "female", 33);
+let student4 = new Student("Alenka", "female", 10);
+
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  return this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+
+
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    return this.marks = [mark];
+  } else {
+    return this.marks.push(mark);
+}
+}
+
+Student.prototype.addMarks = function (...marks) {
+
+    return this.marks.push(...marks);
+
+}
+
+Student.prototype.getAverage = function (){
+  return this.avg = this.marks.reduce((a, b) => (a + b)) / this.marks.length;
+  
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks; 
+  return this.excluded = reason;
+}
+
+
+
+
